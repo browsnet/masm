4e34e27e7b809d51bbdfd2f24e8ac6ee760f4889
browsnet <browsnet@163.com>
2017-10-25 17:47:57 +0800

    remove generated files

--- out/Default/.deps/out/Default/masm.d
+++
@@ -1,1 +0,0 @@
-cmd_out/Default/masm := g++ -pthread  -o out/Default/masm -Wl,--start-group out/Default/obj.target/masm/src/masm.o out/Default/obj.target/deps/libuv/libuv.a -Wl,--end-group -lm -ldl -lrt
--- out/Default/.deps/out/Default/obj.target/libuv/deps/libuv/src/unix/linux-syscalls.o.d
+++
@@ -1,6 +0,0 @@
-cmd_out/Default/obj.target/libuv/deps/libuv/src/unix/linux-syscalls.o := cc '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D_GNU_SOURCE' -Ideps/libuv/include -Ideps/libuv/src  -pthread -fvisibility=hidden -g --std=gnu89 -pedantic -Wall -Wextra -Wno-unused-parameter -Wstrict-aliasing  -MMD -MF out/Default/.deps/out/Default/obj.target/libuv/deps/libuv/src/unix/linux-syscalls.o.d.raw   -c -o out/Default/obj.target/libuv/deps/libuv/src/unix/linux-syscalls.o deps/libuv/src/unix/linux-syscalls.c
-out/Default/obj.target/libuv/deps/libuv/src/unix/linux-syscalls.o: \
- deps/libuv/src/unix/linux-syscalls.c \
- deps/libuv/src/unix/linux-syscalls.h
-deps/libuv/src/unix/linux-syscalls.c:
-deps/libuv/src/unix/linux-syscalls.h:
